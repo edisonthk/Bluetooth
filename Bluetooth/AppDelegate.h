fc733f31ec635d78125331b0f70e0c9fb6b6e3d7
Edisonthk <edisonthk@gmail.com>
2014-04-13 22:13:22 +0900

    Initial Commit

---
+++ Bluetooth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bluetooth
+//
+//  Created by Edisonthk on 4/13/14.
+//  Copyright (c) 2014 Edisonthk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
